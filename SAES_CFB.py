6a0b40379997a3dc509b52eaf6d4716ffd72745b
Zhang Hao <zhangghaoo@gmail.com>
2017-03-30 20:44:12 +0800

    solution

--- SAES_CFB.py
+++ SAES_CFB.py
@@ -1,56 +1,54 @@
 import hashlib
-#Group: HowZ
-#Draft code
+
+# Group: HowZ
+# Draft code
 
 plaintext = 'something'
 data = bytearray()
-data.extend(map(ord,plaintext))
+data.extend(map(ord, plaintext))
 
 fullround = 80
 
 keyplain = 'test'
-hash1 = hashlib.md5(keyplain.encode('utf-8'))  #Generate 128 bits key
+hash1 = hashlib.md5(keyplain.encode('utf-8'))  # Generate 128 bits key
 keyarray = bytearray(hash1.digest())
 
 ivplain = 'hello'
-hash2 = hashlib.md5(ivplain.encode('utf-8'))  #Generate 128 bits iv
+hash2 = hashlib.md5(ivplain.encode('utf-8'))  # Generate 128 bits iv
 iv = bytearray(hash2.digest())
 
 
 def getrounds(data, roundnumber, keyarray, iv):
     state = iv
     for i in range(roundnumber):
-        roundarray = simplifiedAES(keyarray, iv)
+        roundarray = SAES(keyarray, iv)
         statearray = xor(roundarray, data)
 
     return statearray
 
 
+def SAES(iv,keyarray):
+    # todo implement simplified AES encryption method
+    # todo return encryptedResult
+    pass
 
-def simplifiedAES(keyarray):
-    #TO DO
-    #implement simplified AES encryption method
-    #return encryptedResult
-    
 
 def xor(roundarray, data):
     statearray = bytearray()
     for i in range(len(roundarray)):
-        statearray.append(roundarray[i]^data[i])
+        statearray.append(roundarray[i] ^ data[i])
     return statearray
 
 
 statearray = getrounds(data, fullround, keyarray, iv)
 ciphertext = ""
 for i in statearray:
-    ciphertext+=chr(i)  #generate ciphertext
+    ciphertext += chr(i)  # generate ciphertext
 print(ciphertext)
 
-#decryption: replace data with ciphertext
+# decryption: replace data with ciphertext
 result = getrounds(statearray, fullround, keyarray, iv)
 plaintext = ""
 for i in result:
-    plaintext+=chr(i)
+    plaintext += chr(i)
 print(plaintext)
-
-
--- temp.py
+++ temp.py
@@ -3,3 +3,6 @@ import hashlib
 key = hashlib.md5("HowZ".encode('ascii')).hexdigest()
 
 print(key)
+
+
+print("sdlfkjsd".find("hello"))
