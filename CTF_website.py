20274fd90619c77f485465576673f7d269d2bbf1
JennySG <yujia_wang@mymail.sutd.edu.sg>
2017-03-30 20:42:52 +0800

    updated website

---
+++ CTF_website.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# Kopi-CTF Challenge
+# HowZ: Yujia, Hao, Su, Xiongyi
+
+from flask import Flask, render_template,request,redirect,make_response,session
+import sqlite3
+import os
+import hashlib
+
+app = Flask(__name__)
+
+def hash(data):
+    """ Wrapper around sha224 """
+    return hashlib.sha224(data.replace('\n','').encode('ascii')).hexdigest()
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+        
+@app.route('/search')
+def search():
+    term = request.args.get('term')
+    return render_template('main.html', error='haha, you think I have any function here?!')
+
+if __name__ == '__main__':
+    app.config.update(SESSION_COOKIE_HTTPONLY=False)
+    app.run(debug=True)
